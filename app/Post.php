276f69fc2c08c81775335f8274ce66586293bab7
YmNIK13 <valery.stavitsky@gmail.com>
2019-02-12 20:11:36 +0200

    Add Model + Добавил обратную связь с категориями

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,12 @@ class Post extends Model
     protected $guarded = [];
     // protected $fillable = ['title','slug','text','user_id','category_id'];
     //
+
+    /**
+     * Получить категорию.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
