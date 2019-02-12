455cb3581136ed4ff323c87b42d13a2df52a62d5
YmNIK13 <valery.stavitsky@gmail.com>
2019-02-12 18:10:57 +0200

    Добавлен пользователь и категории для выбора во вьюхе

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,27 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
+use App\User;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Input;
 
 class PostsController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-
         return view('posts.index', compact('posts'));
     }
 
     public function create()
     {
-        return view('posts.create');
+        $categories = Category::all(array('id','title'));
+
+        // todo #view Добавить реального юзера
+        $cur_user = new User();
+        $cur_user->id = 1;
+
+        return view('posts.create')->withCategories($categories)->with('user', $cur_user);
     }
 
     public function store()
     {
-        Post::create(request(['title','text','slug','category_id']));
-
+        Post::create(request(['title','slug','text','user_id','category_id']));
         return redirect('/posts');
     }
 
