e41619d89077677c78efd41a40dd455683620720
YmNIK13 <valery.stavitsky@gmail.com>
2019-02-12 18:11:54 +0200

    Фикс массовго добавления https://laravel.com/docs/5.7/eloquent

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    protected $guarded = [];
+    // protected $fillable = ['title','slug','text','user_id','category_id'];
     //
 }
