3e69560de93207eac6474029ae39eef792526d4b
kirmessi <khitsenko@bk.ru>
2019-02-13 18:15:36 +0200

    create layout for categories

--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -1,6 +1,10 @@
-@extends('layouts.app')
+@extends('user.app')
 
-@section('content')
+@section('bg-img',asset('user/img/home-bg.jpg'))
+@section('title', $data['posts'][0]['cat_title'])
+@section('sub-title', '')
+
+@section("main-content")
     <div class="container">
     <h1>Категория: {{ $data['posts'][0]['cat_title'] }}</h1>
     @foreach($data['posts'] as $post)
