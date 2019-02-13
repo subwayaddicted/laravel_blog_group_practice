d7de4e3e6b5ff1bcfed29b0b51f827e392d831b3
subwayaddicted <novitsky.vladislav00@gmail.com>
2019-02-13 15:45:02 +0200

    Posts Controller for Post resource route

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,17 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    /**
-     * Get all posts
-     *
-     * @return void
-     */
-    public function getAllPosts()
+    public function index()
     {
-        echo 'ok';
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        Post::create(request(['title','text','slug','category_id']));
+
+        return redirect('/posts');
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+    public function update(Post $post)
+    {
+        $post->update(['title','text','slug','category_id']);
+
+        return redirect('/posts');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect('/posts');
     }
 }
