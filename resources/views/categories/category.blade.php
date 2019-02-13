47a4444f39704ded19348f4a1c19860c2b2d5cf7
subwayaddicted <novitsky.vladisalv00@gmail.com>
2019-02-13 18:26:21 +0200

    Fixed posts by categories

--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -6,7 +6,7 @@
 
 @section("main-content")
     <div class="container">
-    <h1>Категория: {{ $data['posts'][0]['cat_title'] }}</h1>
+    <h1>Category: {{ $data['posts'][0]['cat_title'] }}</h1>
     @foreach($data['posts'] as $post)
         <div class="container">
             <h3>{{ $post['title'] }}</h3>
