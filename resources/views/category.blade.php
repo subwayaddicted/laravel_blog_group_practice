a3cc692a8e878b0cb2f316fa8e92d35198ead57d
cmsstudent <nevidukvadim@gmail.com>
2019-02-13 15:46:03 +0200

    fix Category 3

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,14 +1,37 @@
-@extends('layouts.app')
 
-@section('content')
+@extends('user.app')
+
+@section('bg-img','/user/img/home-bg.jpg')
+
+@section('head')
+
+
+
+@endsection
+
+@section('title', $data['posts'][0]['cat_title'])
+@section('sub-title', '')
+
+
+
+@section("main-content")
+    <!-- Main Content -->
     <div class="container">
-    <h1>Категория: {{ $data['posts'][0]['cat_title'] }}</h1>
+      <div class="row" id="app">
+        <div class="col-lg-8 col-md-10 mx-auto">
+        
+      <h1>Категория: {{ $data['posts'][0]['cat_title'] }}</h1>
     @foreach($data['posts'] as $post)
         <div class="container">
             <h3>{{ $post['title'] }}</h3>
             <p>{{ $post['text'] }}</p>
-            <a href="/posts/{{ $post['id'] }}">Читать далее...</a>
+            <a href="/{{ $post['slug'] }}">Читать далее...</a>
         </div>
     @endforeach
+        </div>
+      </div>
     </div>
+
+    <hr>
+
 @endsection
